8380efa988c35ee00f259c638dccd0514e5ee9d4
Vasi1i <7pwrrr@gmail.com>
2024-07-26 12:12:04 +0300

    init

---
+++ src/main/java/org/example/client/dto/RecentTempsRequest.java
@@ -0,0 +1,11 @@
+package org.example.client.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class RecentTempsRequest {
+    private int count;
+    private int offset;
+}
